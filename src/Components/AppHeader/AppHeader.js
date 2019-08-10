e1a48afac34e540359dafaba036619e29bfc9fe5
Vikrant Parab <vkrantparab@gmail.com>
2019-08-10 20:45:18 +0530

    Added app header

---
+++ src/Components/AppHeader/AppHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AppHeader.css';
+
+export const AppHeader = () => {
+  return (
+    <div className={'header'}>
+      <h1>This is Header</h1>
+    </div>
+  );
+};
