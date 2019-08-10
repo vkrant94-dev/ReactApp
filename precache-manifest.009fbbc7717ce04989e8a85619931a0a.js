c31b69b2e2a0971c7b2167b5bb295961b34e40b7
Vikrant Parab <vkrantparab@gmail.com>
2019-08-10 20:10:56 +0530

    Updates

---
+++ precache-manifest.009fbbc7717ce04989e8a85619931a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b17271bcd5bc963d93a68b3206d73d",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "92eea60ecda7a713b542",
+    "url": "/ReactApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ede490a44a0ad3754691",
+    "url": "/ReactApp/static/js/2.1c4a84ed.chunk.js"
+  },
+  {
+    "revision": "92eea60ecda7a713b542",
+    "url": "/ReactApp/static/js/main.459df469.chunk.js"
+  },
+  {
+    "revision": "7fdd63652748ec3d5f58",
+    "url": "/ReactApp/static/js/runtime~main.051eebcd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
